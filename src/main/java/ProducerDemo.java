98ffaea0f2d011a2272981c48a7d9aef768394b5
Emanuel2592 <thomasep@mymail.vcu.edu>
2021-05-23 21:30:52 -0400

    kafka practice

--- src/main/java/ProducerDemo.java
+++ src/main/java/ProducerDemo.java
@@ -4,13 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ProducerDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         Logger loggerFactory=LoggerFactory.getLogger(ProducerDemo.class);
         //Set the producer
         Properties properties=new Properties();
+
 
         //Creating the producer properties
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
@@ -21,29 +23,35 @@ public class ProducerDemo {
         //Creating the producer
         KafkaProducer<String,String> producer= new KafkaProducer<String, String>(properties);
 
-        //Create the producer record
-        ProducerRecord<String,String> record=new ProducerRecord<String,String>("first_topic","Hello World");
+
+        for(int i=0;i<=10;i++){
+            //Create the producer record
+
+            String key="id"+Integer.toString(i);
+            ProducerRecord<String,String> record=new ProducerRecord<String,String>("first_topic",key,"Heylo World"+i);
+
+            loggerFactory.info("key"+key);
+            //send data
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes everytime a record is successfully sent or an exception is throw
+                    if(e==null){
+                        loggerFactory.info("\n"+"Received new metadata Topic "+
+                                recordMetadata.topic()+ "\n"+
+                                "Partition "+ recordMetadata.partition() +"\n"
+                                +" Offset"+recordMetadata.offset()+ "\n"+
+                                "TimeStamp"+recordMetadata.timestamp()
+                        );
+                    }   else {
+                        e.printStackTrace();
+                    }
 
 
-        //send data
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                //executes everytime a record is successfully sent or an exception is throw
-                if(e==null){
-                    loggerFactory.info("Received new metadata Topic "+
-                    recordMetadata.topic()+ "\n"+
-                                    "Partition "+ recordMetadata.partition() +"\n"
-                    +" Offset"+recordMetadata.offset()+ "\n"+
-                            "TimeStamp"+recordMetadata.timestamp()
-                    );
-                }   else {
-                    e.printStackTrace();
                 }
+            });
 
-
-            }
-        });
+        }
 
         producer.flush();
 
